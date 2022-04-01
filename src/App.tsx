7e1678fd45eee3961df34842c6d31f0e68e5e35b
Ana Ventura <anaventura1811@gmail.com>
2022-04-01 18:53:23 -0300

    fix: publish app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import './global.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
